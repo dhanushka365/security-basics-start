bdbc0ac3a678bcccf41a83af54a726a69f2bf28e
uduwela96 <puduwel@mitrai.com>
2022-07-23 14:45:46 +0530

    configuring credentials in database

--- src/main/java/com/way2learnonline/WebSecurityConfiguration.java
+++ src/main/java/com/way2learnonline/WebSecurityConfiguration.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -30,23 +34,29 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 	
 	//TODO-11 uncomment the below to configure jdbc authentication using DelegatingPasswordEncoder
 	
-	/*
-	  
+
 	  
 	 @Autowired
 	private DataSource dataSource;
 	
-	
+	/*
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	*/
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		DelegatingPasswordEncoder encoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return encoder;
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
-		.dataSource(dataSource).passwordEncoder(passwordEncoder);
+		.dataSource(dataSource).passwordEncoder(getPasswordEncoder());
 	}	
 	
 	
-	*/
+
 	
 }
