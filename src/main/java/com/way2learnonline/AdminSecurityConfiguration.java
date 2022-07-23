68ee19d9b795e1dc4510e53f46865b38b39a12ab
uduwela96 <puduwel@mitrai.com>
2022-07-23 14:20:50 +0530

    secure the app with digest Authentication

--- src/main/java/com/way2learnonline/AdminSecurityConfiguration.java
+++ src/main/java/com/way2learnonline/AdminSecurityConfiguration.java
@@ -3,33 +3,41 @@ package com.way2learnonline;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
 
 
 @Configuration
 @Order(1)
 // TODO-4 Make the below class extend WebSecurityConfigurerAdapter
 
-public class AdminSecurityConfiguration //extends WebSecurityConfigurerAdapter
+public class AdminSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
 	
 	
 	
 	// TODO-5 uncomment the below to configure DigestAuthenticationEntryPoint
 	
-	/*
+
 	private DigestAuthenticationEntryPoint getDigestEntryPoint() {
 		DigestAuthenticationEntryPoint digestEntryPoint = new DigestAuthenticationEntryPoint();
 		digestEntryPoint.setRealmName("admin-digest-realm");
 		digestEntryPoint.setKey("somedigestkey");
 		return digestEntryPoint;
 	}
-	*/
+
 	
 	
 
 	// TODO-6 uncomment the below PasswordEncoder Bean
 	
-/*
+
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -37,42 +45,41 @@ public class AdminSecurityConfiguration //extends WebSecurityConfigurerAdapter
 		
 		return NoOpPasswordEncoder.getInstance();
 	}
-	
-	*/
+
 	
 	
 	// TODO-7 uncomment the below method to configure authentication manager
 	
-	/*
+
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
         .withUser("digestsiva")
             .password(passwordEncoder().encode("digestsecret"))
             .roles("USER")
     .and()
-        .withUser("admin")
-            .password(passwordEncoder().encode("adminsecret"))
+        .withUser("pasindu")
+            .password(passwordEncoder().encode("123"))
             .roles("ADMIN");
 	}
 	
-	*/
+
 	
 	
 	// TODO-8 uncomment the below UserDetailsService as we cont want the default UserDetailsService configured by  UserDetailsServiceAutoConfiguration
 
-	/*
+
 	@Bean
 	public UserDetailsService userDetailsServiceBean() throws Exception {
 		return super.userDetailsServiceBean();
 	}
 	
-	*/
+
 	
 	
 	// TODO-9 Uncomment the below to configure  DigestAuthenticationFilter.
 		// Observe the we are injecting userDetailsService and DigestAuthenticationEntryPoint
 	
-	/*
+
 		private DigestAuthenticationFilter getDigestAuthFilter() throws Exception {
 			DigestAuthenticationFilter digestFilter = new DigestAuthenticationFilter();
 			
@@ -83,7 +90,7 @@ public class AdminSecurityConfiguration //extends WebSecurityConfigurerAdapter
 			return digestFilter;
 		}
 		
-		*/
+
 		
 	
 	
@@ -93,7 +100,7 @@ public class AdminSecurityConfiguration //extends WebSecurityConfigurerAdapter
 	//Observe how we are customizing the filter chain and adding DigestAuthenticationFilter
 	//Also observe how we are configuring authenticationEntryPoint
 	
-	/*
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	
@@ -104,7 +111,7 @@ public class AdminSecurityConfiguration //extends WebSecurityConfigurerAdapter
 		.and().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
 	}
 	
-	*/
+
 
 	
 	
--- src/main/java/com/way2learnonline/WebSecurityConfiguration.java
+++ src/main/java/com/way2learnonline/WebSecurityConfiguration.java
@@ -6,27 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 @Order(2)
 // TODO-2 Make the below class to extend WebSecurityConfigurerAdapter
-public class WebSecurityConfiguration //extends WebSecurityConfigurerAdapter 
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
 	
 	// TODO-3 Uncomment the below to add httpBasic authentication for your app
-	
-	/*
-	protected void configure(HttpSecurity http) throws Exception {		
+	protected void configure(HttpSecurity http) throws Exception {
 		
 		  http .authorizeRequests() 
 		  		.anyRequest().authenticated() 
 		  		.and() .httpBasic();
 		
 	}
-	
-	*/
+
 	
 	
 	
